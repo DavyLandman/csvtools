cbe39cad99cea80797ca87e3e7463f18c5b97290
Davy Landman <davy.landman@gmail.com>
2015-01-21 09:16:15 +0100

    Working on parsing the csv

--- src/csvchop.c
+++ src/csvchop.c
@@ -24,9 +24,7 @@
 
 
 static Configuration parse_config(int argc, char** argv);
-static ProcessResult parse_cells(void* buffer, size_t buffer_read, size_t already_processed, Cell* cells, size_t max_cells, Configuration* config, Context* context);
 static void output_cells(Cell* cells, int number_of_cells, Configuration* config, Context* context);
-
 int main(int argc, char** argv) {
 	Configuration config = parse_config(argc, argv);
 	Context ctx;
@@ -184,11 +182,6 @@ static Configuration parse_config(int argc, char** argv) {
 	return config;
 }
 
-static ProcessResult parse_cells(void* buffer, size_t buffer_read, size_t already_processed, Cell* cells, size_t max_cells, Configuration* config, Context* context) {
-	ProcessResult result;
-	return result;
-
-}
 static void output_cells(Cell* cells, int number_of_cells, Configuration* config, Context* context) {
 
 }
