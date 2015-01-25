984d300dda58dd34da5a576a66b86d389c7a3cf1
Davy Landman <davy.landman@gmail.com>
2015-01-25 20:57:59 +0100

    Got the code working again

--- src/csvchop.c
+++ src/csvchop.c
@@ -19,87 +19,97 @@
 #define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
 
 #define BUFFER_SIZE 1024*1024
-//#define BUFFER_SIZE 20
+//#define BUFFER_SIZE 30
 #define CELL_BUFFER_SIZE BUFFER_SIZE / 4
 
 #define FREEARRAY(l,c) { for(size_t ___i = 0; ___i< c; ___i++) { free(l[___i]); } }
+
+
+#define debug_print(fmt, ...) do { fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+#ifdef DEBUG
+	#define LOG_D(fmt, ...) debug_print(" D: "fmt, __VA_ARGS__)
+#else
+	#define LOG_D(fmt, ...) 
+#endif
+
+#ifdef MOREDEBUG
+	#define LOG_V(fmt, ...) debug_print(" V: "fmt, __VA_ARGS__)
+#else
+	#define LOG_V(fmt, ...) 
+#endif
+
 
 static char _buffer[BUFFER_SIZE];
 static char const* _cell_starts[CELL_BUFFER_SIZE];
 static size_t _cell_lengths[CELL_BUFFER_SIZE];
 
 // config info
-static size_t _column_count;
-static size_t _current_cell_id;
-static char _separator;
+static int _column_count = 0;
+static int _current_cell_id = 0;
+static char _separator = ',';
 static char _newline[2];
-static size_t _newline_length;
+static size_t _newline_length = 0;
 static char* _keep;
-static size_t _first_cell;
-static bool _half_printed;
+static int _first_cell = 0;
+static bool _half_printed = false;
 
 
 static size_t parse_config(int argc, char** argv, size_t chars_read);
 
 static void output_cells(size_t cells_found, bool last_full);
-#ifdef MOREDEBUG
-static void print_cells(size_t total);
-#endif
+static void debug_cells(size_t total);
 
 int main(int argc, char** argv) {
 	size_t chars_read;
 	bool first = true;
 
 	while ((chars_read = fread(_buffer, 1, BUFFER_SIZE, stdin)) > 0) {
-#ifdef DEBUG
-		fprintf(stderr, "New data read: %zu\n", chars_read);
-#endif
-
+		LOG_D("New data read: %zu\n", chars_read);
 		size_t buffer_consumed = 0;
 		size_t cells_found = 0;
 		bool last_full = true;
+
 		if (first) {
-			first = true;
+			first = false;
 			buffer_consumed = parse_config(argc, argv, chars_read);
+			LOG_D("Finished init : %zu\n", buffer_consumed);
 		}
 
 		while (buffer_consumed < chars_read) {
 			parse_cells(buffer_consumed, chars_read, &buffer_consumed, &cells_found, &last_full);
 
-#ifdef DEBUG
-			fprintf(stderr, "Processed: %zu, Cells: %zu\n", buffer_consumed, cells_found);
-#endif
-#ifdef MOREDEBUG
-			print_cells(cells_found);
-#endif
+			LOG_D("Processed: %zu, Cells: %zu\n", buffer_consumed, cells_found);
+			debug_cells(cells_found);
 			output_cells(cells_found, last_full);
 		}
 	}
 	return 0;
 }
 
+static void debug_cells(size_t total) {
 #ifdef MOREDEBUG
-static void print_cells(size_t total) {
 	char const ** current_cell_start = _cell_starts;
 	char const ** cell_starts_end = _cell_starts + total;
 	size_t* current_cell_length = _cell_lengths;
 
 	while (current_cell_start < cell_starts_end) {
 		if (*current_cell_start == NULL) {
-			fprintf(stderr, "Cell %zu : Newline\n", (size_t)(current_cell_start - _cell_starts));
+			LOG_V("Cell %zu : Newline\n", (size_t)(current_cell_start - _cell_starts));
 		}
 		else {
 			char* s = calloc(sizeof(char), *current_cell_length);
 			s[*current_cell_length + 1] = '\0';
 			memcpy(s, *current_cell_start, *current_cell_length);
-			fprintf(stderr, "Cell %zu : %s\n", (size_t)(current_cell_start - _cell_starts), s);
+			LOG_V("Cell %zu : %s\n", (size_t)(current_cell_start - _cell_starts), s);
 			free(s);
 		}
 		current_cell_start++;
 		current_cell_length++;
 	}
-}
+#else
+	(void)total;
 #endif
+}
 
 static void print_help() {
 	fprintf(stderr, "-s ,\n");
@@ -173,9 +183,7 @@ static size_t parse_config(int argc, char** argv, size_t chars_read) {
 		exit(1);
 	}
 
-#ifdef DEBUG
-	fprintf(stderr, "Done parsing config params\n");	
-#endif 
+	LOG_D("%s\n","Done parsing config params");	
 
 	setup_parser(_separator, _buffer, _cell_starts, _cell_lengths, CELL_BUFFER_SIZE);
 
@@ -183,18 +191,14 @@ static size_t parse_config(int argc, char** argv, size_t chars_read) {
 	bool last_full;
 	parse_cells(0, chars_read, &consumed, &cells_found, &last_full);
 
-#ifdef DEBUG
-	fprintf(stderr, "Processed: %zu, Cells: %zu\n", consumed, cells_found);
-#endif
-#ifdef MOREDEBUG
-	print_cells(cells_found);
-#endif
+	LOG_D("Processed: %zu, Cells: %zu\n", consumed, cells_found);
+	debug_cells(cells_found);
 
 	char const** current_cell = _cell_starts;
 	while (current_cell < (_cell_starts + cells_found) && *current_cell != NULL) {
 		current_cell++;
 	}
-	_column_count = (size_t)(current_cell - _cell_starts);
+	_column_count = (int)(current_cell - _cell_starts);
 
 	const char* new_line = _cell_starts[_column_count-1] + _cell_lengths[_column_count - 1];
 	_newline[0] = new_line[0];
@@ -209,7 +213,7 @@ static size_t parse_config(int argc, char** argv, size_t chars_read) {
 
 	if (keeps || drops) {
 		const char** chops = (const char**)(keeps ? keeps : drops);
-		for (size_t c = 0; c < _column_count; c++) {
+		for (int c = 0; c < _column_count; c++) {
 			bool cont = contains_n(chops, chops_size, _cell_starts[c], _cell_lengths[c]);
 			if ((cont && keeps) || (!cont && drops)) {
 				BITSET(_keep, c);
@@ -222,14 +226,14 @@ static size_t parse_config(int argc, char** argv, size_t chars_read) {
 		}
 	}
 	else if (index_drops) {
-		for (size_t c = 0; c < _column_count; c++) {
+		for (int c = 0; c < _column_count; c++) {
 			BITSET(_keep, c);
 		}
 		for (size_t i = 0; i < chops_size; i++) {
 			BITCLEAR(_keep, index_drops[i]);
 		}
 	}
-	for (size_t c = 0; c < _column_count; c++) {
+	for (int c = 0; c < _column_count; c++) {
 		if (BITTEST(_keep, c)) {
 			_first_cell = c;
 			break;
@@ -257,22 +261,24 @@ static size_t parse_config(int argc, char** argv, size_t chars_read) {
 }
 
 static void output_cells(size_t cells_found, bool last_full) {
+	LOG_D("Starting output: %zu (%d)\n", cells_found, last_full);
+	LOG_V("Entry: current_cell: %ld\n", _current_cell_id);
 	char const ** current_cell_start = _cell_starts;
 	char const ** cell_starts_end = _cell_starts + cells_found;
 	size_t* current_cell_length = _cell_lengths;
 	while (current_cell_start < cell_starts_end) {
-			if (_current_cell_id > _column_count) {
-				fprintf(stderr, "To many cells in this row, expect: %zu, got: %zu\n", _column_count, _current_cell_id);
-				exit(1);
-				return;
-			}
+		if (_current_cell_id > _column_count) {
+			fprintf(stderr, "Too many cells in this row, expect: %d, got: %d\n", _column_count, _current_cell_id);
+			exit(1);
+			return;
+		}
 		if (*current_cell_start == NULL) {
 			if (_current_cell_id == _column_count) {
 				fwrite(_newline, sizeof(char), _newline_length, stdout);
 				_current_cell_id = -1;
 			}
 			else if (_current_cell_id < _column_count) {
-				fprintf(stderr, "Not enough cells in this row, expect: %zu, got: %zu\n", _column_count, _current_cell_id);
+				fprintf(stderr, "Not enough cells in this row, expect: %d, got: %d\n", _column_count, _current_cell_id);
 				exit(1);
 				return;
 			}
@@ -285,15 +291,17 @@ static void output_cells(size_t cells_found, bool last_full) {
 			}
 			fwrite(*current_cell_start, sizeof(char), *current_cell_length, stdout);
 		}
+
 		_current_cell_id++;
 		current_cell_start++;
 		current_cell_length++;
 	}
-	if (last_full) {
+	if (!last_full) {
 		_half_printed = true;
 		_current_cell_id--;
 	}
 	else {
 		_half_printed = false;
 	}
+	LOG_V("Exit: current_cell: %ld\n", _current_cell_id);
 }
