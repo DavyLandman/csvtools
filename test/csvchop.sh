cf5bbaee906c92d3398cb137a7f10aa06f1d6961
Davy Landman <davy.landman@gmail.com>
2015-01-25 16:34:02 +0100

    Added some testing for certain cases
    This found some bugs in the parser

---
+++ test/csvchop.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+RESULT=0
+
+function test_csvchop() {
+	REF=`cat test-data/$1 | csvcut $3`
+	if (($? > 0)); then
+		echo "Test failed: " $1 " : " $2 " (csvcut failed)" 
+		RESULT=1
+		return
+	fi
+
+	OUTPUT=`cat test-data/$1 | ./csvchop $2`
+	if (($? > 0)); then
+		echo "Test failed: " $1 " : " $2 " (csvchop failed)" 
+		RESULT=1
+		return
+	fi
+
+
+	if [ "$OUTPUT" != "$REF" ]; then
+		echo "Test failed: " $1 " : " $2
+		RESULT=1
+	fi
+}
+
+function test_csvchop_xz() {
+	REF=`xzcat test-data/$1 | csvcut $3 | md5sum`
+	if (($? > 0)); then
+		echo "Test failed: " $1 " : " $2 " (csvcut failed)" 
+		RESULT=1
+		return
+	fi
+	OUTPUT=`xzcat test-data/$1 | ./csvchop $2 | md5sum`
+	if (($? > 0)); then
+		echo "Test failed: " $1 " : " $2 " (csvchop failed)" 
+		RESULT=1
+		return
+	fi
+	if [ "$OUTPUT" != "$REF" ]; then
+		echo "Test failed: " $1 " : " $2
+		RESULT=1
+	fi
+}
+
+test_csvchop "simple.csv" "-d a" "-C a" 
+test_csvchop "simple.csv" "-d a,b" "-C a,b" 
+test_csvchop "simple.csv" "-k a,b" "-c a,b" 
+test_csvchop "large-fields.csv" "-k column1" "-c column1" 
+test_csvchop "large-fields.csv" "-k column2,column3" "-c column2,column3" 
+test_csvchop "large-fields.csv" "-K 1,2" "-c column2,column3" 
+
+test_csvchop_xz "canada-2011-census.csv.xz" "-d Note" "-C Note" 
+
+if [ $RESULT == 0 ]; then
+	echo "Tests succeeded"
+else
+	echo "Tests failed"
+fi
+exit $RESULT
