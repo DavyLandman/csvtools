65b87060eac92fbb1cb2b455685dee1b02dc651c
Davy Landman <Davy.Landman@cwi.nl>
2015-02-15 21:41:52 +0100

    The csvgrep has issues with partial matches

--- test/csvgrep.sh
+++ test/csvgrep.sh
@@ -49,10 +49,10 @@ test_csvgrep "simple.csv" "-p a/[0-9]+/" "-c a -r [0-9]+"
 test_csvgrep "simple.csv" "-p a/[1-2]/ -p b/[2-3]/" "-c a,b -r [1-3]" 
 test_csvgrep "corners.csv" "-p a/^$/" "-c a -r ^$" 
 test_csvgrep "corners.csv" "-p 'a/[\"]/'" "-c a -r [\"]" # let's test the advanced un-escaping
-test_csvgrep "large-fields.csv" "-p 'column1/(foo|bar)/'" "-c column1 -r (foo|bar)" 
-test_csvgrep "large-fields.csv" "-p 'column3/(foo|bar)/'" "-c column3 -r (foo|bar)" 
+test_csvgrep "large-fields.csv" "-p 'column1/(foo|bar)/'" "-c column1 -r .*(foo|bar).*" 
+test_csvgrep "large-fields.csv" "-p 'column3/(foo|bar)/'" "-c column3 -r .*(foo|bar).*" 
 
-test_csvgrep_xz "canada-2011-census.csv.xz" "-p Characteristic/201[0-2]/" "-c Characteristic -r 201[0-2]" 
+test_csvgrep_xz "canada-2011-census.csv.xz" "-p Characteristic/201[0-2]/" "-c Characteristic -r .*201[0-2].*" 
 
 if [ $RESULT == 0 ]; then
 	echo "Tests succeeded"
