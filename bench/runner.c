1d06518dbe676732c826d50d71f7d19063d94590
Davy Landman <davy.landman@gmail.com>
2015-10-26 14:08:47 +0100

    Added enlargement feature to re-use the buffer multiple times

--- bench/runner.c
+++ bench/runner.c
@@ -9,13 +9,15 @@ static void print_help() {
     fprintf(stderr,"options:");
     fprintf(stderr, "-b 200\n");
     fprintf(stderr, "\tbench size in MBs\n");
+    fprintf(stderr, "-e 2\n");
+    fprintf(stderr, "\tenlarge bench size by repeating it x times\n");
     fprintf(stderr, "-c 6\n");
     fprintf(stderr, "\tcolumns to generate\n");
     fprintf(stderr, "-r 5\n");
     fprintf(stderr, "\tnumber of measure runs\n");
 }
 
-static void run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int repeats, double* results) {
+static void run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int buffer_copy, unsigned int repeats, double* results) {
     for (unsigned int r = 0; r < repeats; r++) {
         FILE* target = popen(command, "w");
         if (!target) {
@@ -23,7 +25,10 @@ static void run(const char* restrict command, const char* restrict buffer, size_
             results[r] = -1;
         }
         double start = getRealTime();
-        fwrite(buffer, sizeof(char), buffer_size, target);
+        for (unsigned int b = 0; b < buffer_copy; b++) {
+            fwrite(buffer, sizeof(char), buffer_size, target);
+            fflush(target);
+        }
         if (pclose(target) != 0) {
             fprintf(stderr, "\"%s\" had an error.\n", command);
             results[r] = -1;
@@ -33,12 +38,12 @@ static void run(const char* restrict command, const char* restrict buffer, size_
     }
 }
 
-static void print_run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int repeats) {
+static void print_run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int buffer_copy, unsigned int repeats) {
     double* results = calloc(repeats, sizeof(double));
-    run(command, buffer, buffer_size, repeats, results);
+    run(command, buffer, buffer_size, buffer_copy, repeats, results);
     fprintf(stdout, "\"%s\"", command);
     for (unsigned int r = 0; r < repeats; r++) {
-        fprintf(stdout, ",%f", ( buffer_size / results[r]) / (1024*1024) );
+        fprintf(stdout, ",%f", ( (buffer_size * buffer_copy) / results[r]) / (1024*1024) );
     }
     fprintf(stdout, "\n");
     free(results);
@@ -48,9 +53,10 @@ int main(int argc, char** argv) {
     size_t bench_size = 200 * 1024 * 1024; 
     unsigned int columns = 6;
     unsigned int repeats = 5;
+    unsigned int bench_copy = 2;
 
     char c;
-    while ((c = getopt (argc, argv, "b:c:r:h")) != -1) {
+    while ((c = getopt (argc, argv, "b:c:r:e:h")) != -1) {
         switch (c) {
             case 'b':
                 sscanf(optarg, "%zd", &bench_size);
@@ -61,6 +67,9 @@ int main(int argc, char** argv) {
                 break;
             case 'r':
                 sscanf(optarg, "%u", &repeats);
+                break;
+            case 'e':
+                sscanf(optarg, "%u", &bench_copy);
                 break;
             case '?':
             case 'h':
@@ -79,8 +88,9 @@ int main(int argc, char** argv) {
         fprintf(stdout, ",run%u", r);
     }
     fprintf(stdout, "\n");
-    print_run("wc -l > /dev/null", buffer, data_filled, repeats);
-    print_run("wc -l > /dev/null", buffer, data_filled, repeats);
+    fprintf(stderr, "Running csvgrep\n");
+    print_run("wc -l > /dev/null", buffer, data_filled, bench_copy, repeats);
+    print_run("wc -l > /dev/null", buffer, data_filled, bench_copy, repeats);
     
     return 0;
 }
