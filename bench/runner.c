3ad8582920bebd4b1a59e942665d31d97ac899c8
Davy Landman <davy.landman@gmail.com>
2015-10-26 13:15:48 +0100

    Now we can run and time commands

--- bench/runner.c
+++ bench/runner.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "timer.h"
 #include "generate.h"
 
 static void print_help() {
@@ -12,6 +13,35 @@ static void print_help() {
     fprintf(stderr, "\tcolumns to generate\n");
     fprintf(stderr, "-r 5\n");
     fprintf(stderr, "\tnumber of measure runs\n");
+}
+
+static void run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int repeats, double* results) {
+    for (unsigned int r = 0; r < repeats; r++) {
+        FILE* target = popen(command, "w");
+        if (!target) {
+            fprintf(stderr, "Can't start \"%s\"\n", command);
+            results[r] = -1;
+        }
+        double start = getRealTime();
+        fwrite(buffer, sizeof(char), buffer_size, target);
+        if (pclose(target) != 0) {
+            fprintf(stderr, "\"%s\" had an error.\n", command);
+            results[r] = -1;
+        }
+        double stop = getRealTime();
+        results[r] = (stop - start);
+    }
+}
+
+static void print_run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int repeats) {
+    double* results = calloc(repeats, sizeof(double));
+    run(command, buffer, buffer_size, repeats, results);
+    fprintf(stdout, "\"%s\"", command);
+    for (unsigned int r = 0; r < repeats; r++) {
+        fprintf(stdout, ",%fMB/s", ( buffer_size / results[r]) / (1024*1024) );
+    }
+    fprintf(stdout, "\n");
+    free(results);
 }
 
 int main(int argc, char** argv) {
@@ -41,9 +71,11 @@ int main(int argc, char** argv) {
         }
     }
     char* buffer = calloc(bench_size, sizeof(char));
-    fprintf(stderr, "Filling buffer %lu\n",bench_size); 
+    fprintf(stderr, "Preparing data (%zd bytes)\n",bench_size); 
     size_t data_filled = generate_csv(buffer, bench_size, 42, 11, columns);
-    fprintf(stderr, "Printing buffer %lu\n",data_filled); 
-    fwrite(buffer, sizeof(char), data_filled, stdout);
+    fprintf(stderr, "Data ready (%zd bytes)\n",data_filled); 
+    print_run("wc -l > /dev/null", buffer, data_filled, repeats);
+    print_run("wc -l > /dev/null", buffer, data_filled, repeats);
+    
     return 0;
 }
