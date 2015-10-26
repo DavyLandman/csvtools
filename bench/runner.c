435c4a3799961e0a8540b9e5743fa73e568ad5a1
Davy Landman <davy.landman@gmail.com>
2015-10-26 14:23:44 +0100

    Added custom seed setting

--- bench/runner.c
+++ bench/runner.c
@@ -15,6 +15,8 @@ static void print_help() {
     fprintf(stderr, "\tcolumns to generate\n");
     fprintf(stderr, "-r 5\n");
     fprintf(stderr, "\tnumber of measure runs\n");
+    fprintf(stderr, "-s 42\n");
+    fprintf(stderr, "\tseed for random generator\n");
 }
 
 static void run(const char* restrict command, const char* restrict buffer, size_t buffer_size, unsigned int buffer_copy, unsigned int repeats, double* results) {
@@ -49,14 +51,31 @@ static void print_run(const char* restrict command, const char* restrict buffer,
     free(results);
 }
 
+// based on xxhash avalanche
+#define PRIME1   2654435761U
+#define PRIME2   2246822519U
+#define PRIME3   3266489917U
+
+static unsigned int xxh_mix(unsigned int x, unsigned int seed) {
+    unsigned int crc = x  + seed + PRIME1;
+    crc ^= crc >> 15;
+    crc *= PRIME2;
+    crc ^= crc >> 13;
+    crc *= PRIME3;
+    crc ^= crc >> 16;
+    return crc;
+}
+
 int main(int argc, char** argv) {
     size_t bench_size = 200 * 1024 * 1024; 
     unsigned int columns = 6;
     unsigned int repeats = 5;
     unsigned int bench_copy = 2;
+    unsigned int seed1 = xxh_mix(29, 42);
+    unsigned int seed2 = xxh_mix(13, 11);
 
     char c;
-    while ((c = getopt (argc, argv, "b:c:r:e:h")) != -1) {
+    while ((c = getopt (argc, argv, "b:c:r:e:s:h")) != -1) {
         switch (c) {
             case 'b':
                 sscanf(optarg, "%zd", &bench_size);
@@ -71,6 +90,10 @@ int main(int argc, char** argv) {
             case 'e':
                 sscanf(optarg, "%u", &bench_copy);
                 break;
+            case 's':
+                sscanf(optarg, "%u", &seed1);
+                seed1 = xxh_mix(seed1, 42);
+                break;
             case '?':
             case 'h':
             default:
@@ -81,7 +104,7 @@ int main(int argc, char** argv) {
     }
     char* buffer = calloc(bench_size, sizeof(char));
     fprintf(stderr, "Preparing data (%zd bytes)\n",bench_size); 
-    size_t data_filled = generate_csv(buffer, bench_size, 42, 11, columns);
+    size_t data_filled = generate_csv(buffer, bench_size, seed1, seed2, columns);
     fprintf(stderr, "Data ready (%zd bytes)\n",data_filled); 
     fprintf(stdout, "command");
     for (unsigned int r = 0; r < repeats; r++) {
