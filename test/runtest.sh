df68bfdba13b1937e4041e725e000b20b601d96a
Davy Landman <davy.landman@gmail.com>
2016-06-15 17:31:40 +0200

    Revert "No custom codecov"
    This reverts commit 0e0d556470c776697f6bbd00ccb073d103b47ac9.

--- test/runtest.sh
+++ test/runtest.sh
@@ -3,6 +3,20 @@ PROGRAM=$1
 LARGE_FILES=$2
 RESULT=0
 
+DO_COVERAGE=false
+if [ "$#" -eq 2 ] && [ "$LARGE_FILES" = 1 ]; then
+    DO_COVERAGE=true
+    echo "Downloading codecov code"
+    curl -s https://codecov.io/bash > /tmp/codecov.sh
+fi
+
+report_coverage() {
+    if [ "$DO_COVERAGE" = true ] ; then
+        cd ..
+        echo "Reporting to codecov"
+        bash /tmp/codecov.sh -b "normal-test-$PROGRAM" 
+    fi
+}
 
 test_normal() {
     REF_FILE=$OUTPUT
@@ -72,4 +86,5 @@ if [ $RESULT == 0 ]; then
 else
 	printf "Tests failed\n"
 fi
+report_coverage
 exit $RESULT
